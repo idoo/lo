378ddcd55dc5d1b6f1cefc32fcac70e173769be4
Ivan Verevkin <vereva@x-root.org>
2013-03-05 18:26:03 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loco::Application.config.secret_token = '13786442fd5da4017a708b61b4a501334c22c0a2e4c839cce2e9843c0b9db6b3cb1db998fbf7816caaa757082d9ced709728264995170654bd84041db3ba16b6'
